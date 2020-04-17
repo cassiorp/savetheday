df7f300857298d2e732e50f802ef3ddf7acc608a
cassio <cassioribeiropereira@gmail.com>
2020-04-17 11:06:50 -0300

    remover backend nao utilizado.

--- backend/salveodia/src/main/java/br/com/cassio/salveodia/ProdutoService.java
+++
@@ -1,8 +0,0 @@
-package br.com.cassio.salveodia;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class ProdutoService {
-    @Autowired
-    private ProdutoRepository repository;
-}
